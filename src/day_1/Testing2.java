0e4fc038c2cc794d44132d4274b15e723e3240e1
Abdulla Nazarov <64137152+abunazariy@users.noreply.github.com>
2020-04-28 15:48:00 -0400

    Update Testing2.java

--- src/day_1/Testing2.java
+++ src/day_1/Testing2.java
@@ -4,5 +4,9 @@ public class Testing2 {
     public static void main(String[] args) {
         // Testing
         // Testing 123
+        
+        
+        
+        // I made some changes in my Testing2 class from Github
     }
 }
