2aa1ec002c242cab87104b7711bd05a2af6c6fab
Abdulla Nazarov <abunazariy@gmail.com>
2020-04-28 12:57:31 -0400

    add AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,5 +3,8 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
         System.out.println("Hello Git World");
+
+        System.out.println("Hello GitHub World");
+
     }
 }
