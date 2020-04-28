60cdea027afaeda084a118a71fce23c7678eae0e
Abdulla Nazarov <64137152+abunazariy@users.noreply.github.com>
2020-04-28 16:05:59 -0400

    Update HelloWorld.java

--- src/day_1/HelloWorld.java
+++ src/day_1/HelloWorld.java
@@ -3,5 +3,10 @@ package day_1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Second code for GitHub");
+        
+        
+        
+        
+        // I made this changes from Github
     }
 }
